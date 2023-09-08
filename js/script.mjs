cec0b842ca0094147d392a99e2041aa4cf515c9b
Rizki Susilo Ramadhan <rizkysr90@gmail.com>
2023-09-08 18:17:31 +0700

    add: new questions

--- js/script.mjs
+++ js/script.mjs
@@ -1,5 +1,18 @@
-var yourName = "Rizki";
-console.log("Hello ", yourName);
-console.log("Hello it's mee");
-console.log("ninoino");
-console.log("BANG UDAH BANGG");
+var yourName = prompt("What is your name");
+console.log("Halo ".concat(yourName));
+var q1;
+q1 = String(prompt("Mata-mata apa yang nggak bisa pake kacamata?"));
+if (q1Answer(q1)) {
+    console.log("Yeay benarrr");
+}
+else {
+    console.log("jawabannya salah");
+}
+function q1Answer(answer) {
+    answer = answer.toLowerCase();
+    var keyAnswer = "mata kaki";
+    if (answer === keyAnswer) {
+        return true;
+    }
+    return false;
+}
--- ts/script.ts
+++ ts/script.ts
@@ -1,5 +1,17 @@
-let yourName: string = "Rizki";
-console.log("Hello ", yourName);
-console.log("Hello it's mee");
-console.log("ninoino");
-console.log("BANG UDAH BANGG");
+let yourName = prompt("What is your name");
+console.log(`Halo ${yourName}`);
+let q1: string;
+q1 = String(prompt("Mata-mata apa yang nggak bisa pake kacamata?"));
+if (q1Answer(q1)) {
+  console.log("Yeay benarrr");
+} else {
+  console.log("jawabannya salah");
+}
+function q1Answer(answer: string): boolean {
+  answer = answer.toLowerCase();
+  const keyAnswer = "mata kaki";
+  if (answer === keyAnswer) {
+    return true;
+  }
+  return false;
+}
